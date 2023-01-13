b4df5640f4c67c99874d0095884087bc45792ea6
Br3ckon <122642174+Br3ckon@users.noreply.github.com>
2023-01-13 19:47:26 +0000

    new files added

---
+++ java.js
@@ -0,0 +1,15 @@
+function answerA() {
+
+    console.log("No")
+    answer.innerText = "NO"
+}
+
+
+function answerB() {
+
+    console.log("yes")
+  answer.innerText = "YES"
+}
+
+
+let answer = document.getElementById("answer")
